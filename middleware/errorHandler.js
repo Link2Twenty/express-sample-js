a12bc42f9e229c463118c92f3aad6c398eb7c5bf
Andrew Bone <AndrewB05@gmail.com>
2025-03-30 17:48:08 +0100

    📝 create docs

--- middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-export default function errorHandler(err, _req, res, _next) {
-  const status = err?.status || 500;
-  const error = status === 500 ? 'Internal Server Error' : err?.message || 'Internal Server Error';
-
-  if (status === 500) console.error(err.stack);
-
-  res.status(status).json({ data: null, error });
-}
