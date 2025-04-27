622fe9ac94d3008eecd3078ee0d9fb54ee6404ae
Andrew Bone <AndrewB05@gmail.com>
2025-04-27 17:10:28 +0100

    🐛 Get status from cause

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 export default function errorHandler(err, _req, res, _next) {
-  const status = err?.status || 500;
+  const status = err?.cause?.status || 500;
   const error = status === 500 ? 'Internal Server Error' : err?.message || 'Internal Server Error';
 
   if (status === 500) console.error(err.stack);
